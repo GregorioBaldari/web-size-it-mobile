a078ae57ce0c07dc881e20f31be066285cb1870b
GregorioBaldari <gregorio.baldari@gmail.com>
2015-10-01 22:50:46 +0100

    Poiting to Heroku
    server heroku on calm-headland-8821

--- src/js/app.js
+++ src/js/app.js
@@ -32,7 +32,7 @@ mainApp.service('appVars', function () {
 });
 
 mainApp.factory('socket', ['$rootScope', function ($rootScope) {
-  var socket = io('http://localhost:3000');
+  var socket = io("https://secret-lake-6472.herokuapp.com/");
 
   return {
     on: function (eventName, callback) {
