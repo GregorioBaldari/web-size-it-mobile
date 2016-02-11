c85dc63654282bbf66ab8aa3bf1eccc6760d7462
GregorioBaldari <gregorio.baldari@gmail.com>
2016-02-11 19:43:47 +0000

    Socketio Problem Fixed

--- src/js/app.js
+++ src/js/app.js
@@ -7,26 +7,14 @@ var mainApp = angular.module('mainApp', [
 //Change socket configuration to link to server on local or remote.
 //To use server on local use 'node server.js' command
 mainApp.factory('socket', ['$rootScope', function ($rootScope) {
-    var socket = io("https://wesizeit.herokuapp.com");
-    //var socket = io('http://localhost:3000');
+    //var socket = io("https://wesizeit.herokuapp.com");
+    var socket = io('http://localhost:3000');
   return {
       
     getSocket : function () {
         return socket;
     },
-      
-    join: function (room) {
-        return function () {
-            socket.join(room);
-        };
-    },
-      
-    broadcast: function(room,event, data) {
-        return function () {
-            socket.broadcast.to(room).emit(event, data);
-        };
-    },
-      
+
     on: function (eventName, callback) {
         function wrapper() {
             var args = arguments;
